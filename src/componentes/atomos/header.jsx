f4172217953ff417b2edf5beaec0f5f84eb5c69a
David Ruiz <jesuruga@gmail.com>
2023-02-22 22:44:14 -0600

    [add] Create

---
+++ src/componentes/atomos/header.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+import "../../assets/styles/header.css"
+import Logo from "../../assets/images/JasaiART.png"
+
+
+function Header() {
+    return (  
+        <header>
+            
+            <div className="he">
+                <img src={Logo} className="logo" alt="" />
+                <Link to="/Login">
+                <h3 id="Login" > Login </h3>
+                </Link>
+                <Link to="/Sobre-Pintores">
+                    <h3 id="SobreP">Sobre Pintores</h3>
+                </Link>
+                <Link to="/Contacto">
+                    <h3 id="contacto">Contacto</h3>
+                </Link>
+                <Link to="/Carrito">
+                    <h3 id="carrito">Carrito</h3>
+                </Link>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Header from "../componentes/atomos/header";
+import Footer from "../componentes/atomos/footer";
+import ImgText from "../componentes/moleculas/imagen1";
+import "../assets/styles/home.css"
+
+
+function Home() {
+    return ( 
+        <>
+        <Header></Header>
+        <Footer></Footer>
+        <ImgText></ImgText>
+        </>
+     );
+}
+
+export default Home;
